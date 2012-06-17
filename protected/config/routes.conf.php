7badbba5913672763db1bae1eaaf0cec4910c922
Bharad <star@bharad.net>
2012-06-16 18:02:42 -0700

    Added main controller and error controller to routes

--- protected/config/routes.conf.php
+++ protected/config/routes.conf.php
@@ -1,5 +1,1 @@
-<?php
-
-$route['get']['/zip/:zipcode'] = array('EventController', 'events');$route['get']['/event/:event_id'] = array('EventController', 'event');$route['post']['/new'] = array('EventController', 'new');$route['get']['/flag/:event_id'] = array('EventController', 'flag');$route['get']['/like/:event_id'] = array('EventController', 'like');
-
-?>
+<?php$route['*']['/'] = array('MainController', 'index');$route['*']['/error'] = array('ErrorController', 'index');$route['get']['/zip/:zipcode'] = array('EventController', 'events');$route['get']['/event/:event_id'] = array('EventController', 'event');$route['post']['/new'] = array('EventController', 'new');$route['get']['/flag/:event_id'] = array('EventController', 'flag');$route['get']['/like/:event_id'] = array('EventController', 'like');?>
