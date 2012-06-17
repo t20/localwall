e9e7b1dee93de43583df78b9c367e6e504d9dfe5
Tony Lea <tnylea@gmail.com>
2012-06-16 17:19:07 -0700

    Added the event controller

---
+++ protected/controller/EventController.php
@@ -0,0 +1,26 @@
+<?php
+
+class EventController extends DooController {
+
+	function events() {
+		echo 'You are visiting '.$_SERVER['REQUEST_URI'];
+	}
+
+	function event() {
+		echo 'You are visiting '.$_SERVER['REQUEST_URI'];
+	}
+
+	function flag() {
+		echo 'You are visiting '.$_SERVER['REQUEST_URI'];
+	}
+
+	function like() {
+		echo 'You are visiting '.$_SERVER['REQUEST_URI'];
+	}
+
+	function new() {
+		echo 'You are visiting '.$_SERVER['REQUEST_URI'];
+	}
+
+}
+?>
